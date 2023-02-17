c77acc345abec7ce548db189276afac314a2547c
George Beshay <georgebeshay101@gmail.com>
2023-02-17 20:32:06 +0200

    Delete src/intro_to_design_patterns directory

--- src/intro_to_design_patterns/FlyBehavior.java
+++
@@ -1,7 +0,0 @@
-package intro_to_design_patterns;
-
-public interface FlyBehavior {
-	
-	public void fly();
-
-}
--- src/intro_to_design_patterns/QuackBehavior.java
+++
@@ -1,7 +0,0 @@
-package intro_to_design_patterns;
-
-public interface QuackBehavior {
-	
-	public void quack();
-
-}
